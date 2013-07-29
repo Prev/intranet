e026020009cceff4edddc0d0e8400f0c19b1c7e3
Young Soo, Lee <prevdev@gmail.com>
2013-07-29 17:43:59 +0900

    4rd commit

--- config/initialize.sql
+++ config/initialize.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 호스트: localhost
--- 처리한 시간: 13-07-28 20:39
+-- 처리한 시간: 13-07-29 17:03
 -- 서버 버전: 5.1.41-community
 -- PHP 버전: 5.2.12
 
@@ -17,18 +17,18 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- 데이터베이스: `pmc_test`
+-- 데이터베이스: `intranet`
 --
-CREATE DATABASE IF NOT EXISTS `pmc_test` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `pmc_test`;
+CREATE DATABASE IF NOT EXISTS `intranet` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `intranet`;
 
 -- --------------------------------------------------------
 
 --
--- 테이블 구조 `pmc_article`
+-- 테이블 구조 `intra_article`
 --
 
-CREATE TABLE IF NOT EXISTS `pmc_article` (
+CREATE TABLE IF NOT EXISTS `intra_article` (
   `no` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `board_id` int(11) unsigned NOT NULL,
   `category` varchar(20) DEFAULT NULL,
@@ -50,10 +50,10 @@ CREATE TABLE IF NOT EXISTS `pmc_article` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=16 ;
 
 --
--- 테이블의 덤프 데이터 `pmc_article`
+-- 테이블의 덤프 데이터 `intra_article`
 --
 
-INSERT INTO `pmc_article` (`no`, `board_id`, `category`, `title`, `content`, `writer_id`, `top_no`, `order_key`, `is_secret`, `is_notice`, `allow_comment`, `upload_time`, `hits`, `files`) VALUES
+INSERT INTO `intra_article` (`no`, `board_id`, `category`, `title`, `content`, `writer_id`, `top_no`, `order_key`, `is_secret`, `is_notice`, `allow_comment`, `upload_time`, `hits`, `files`) VALUES
 (1, 1, NULL, '안녕하세요. 게시판을 오픈했습니다', '컨텐츠', 1, 1, NULL, 0, 1, 1, '2013-03-06 08:40:14', 0, NULL),
 (2, 1, NULL, '알려드립니다', 'ㅇㅇ', 1, 2, NULL, 0, 0, 1, '2013-03-20 12:51:29', 0, NULL),
 (3, 1, NULL, '질문있습니다.', 'ㅇㅁㄴㅇ', 1, 2, 'AA', 0, 0, 1, '2013-03-20 12:52:47', 0, NULL),
@@ -68,10 +68,10 @@ INSERT INTO `pmc_article` (`no`, `board_id`, `category`, `title`, `content`, `wr
 -- --------------------------------------------------------
 
 --
--- 테이블 구조 `pmc_article_comment`
+-- 테이블 구조 `intra_article_comment`
 --
 
-CREATE TABLE IF NOT EXISTS `pmc_article_comment` (
+CREATE TABLE IF NOT EXISTS `intra_article_comment` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `article_no` int(11) unsigned NOT NULL,
   `content` tinytext NOT NULL,
@@ -83,19 +83,19 @@ CREATE TABLE IF NOT EXISTS `pmc_article_comment` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
--- 테이블의 덤프 데이터 `pmc_article_comment`
+-- 테이블의 덤프 데이터 `intra_article_comment`
 --
 
-INSERT INTO `pmc_article_comment` (`id`, `article_no`, `content`, `writer_id`, `regtime`) VALUES
+INSERT INTO `intra_article_comment` (`id`, `article_no`, `content`, `writer_id`, `regtime`) VALUES
 (1, 2, 'ㅇㅁㄴㅇ', 1, '2013-03-20 12:51:39');
 
 -- --------------------------------------------------------
 
 --
--- 테이블 구조 `pmc_board`
+-- 테이블 구조 `intra_board`
 --
 
-CREATE TABLE IF NOT EXISTS `pmc_board` (
+CREATE TABLE IF NOT EXISTS `intra_board` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
   `name_locales` tinytext NOT NULL,
@@ -109,20 +109,20 @@ CREATE TABLE IF NOT EXISTS `pmc_board` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
--- 테이블의 덤프 데이터 `pmc_board`
+-- 테이블의 덤프 데이터 `intra_board`
 --
 
-INSERT INTO `pmc_board` (`id`, `name`, `name_locales`, `categorys`, `read_permission`, `comment_permission`, `write_permission`, `extra_vars`) VALUES
+INSERT INTO `intra_board` (`id`, `name`, `name_locales`, `categorys`, `read_permission`, `comment_permission`, `write_permission`, `extra_vars`) VALUES
 (1, 'freeboard', '{"en":"Freeboard", "kr":"자유게시판"}', NULL, '["*"]', '["*"]', '["*"]', NULL),
 (2, 'notice', '{"en":"Notice", "kr":"공지사항"}', NULL, '["*"]', '["*"]', '["*"]', NULL);
 
 -- --------------------------------------------------------
 
 --
--- 테이블 구조 `pmc_files`
+-- 테이블 구조 `intra_files`
 --
 
-CREATE TABLE IF NOT EXISTS `pmc_files` (
+CREATE TABLE IF NOT EXISTS `intra_files` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `is_binary` tinyint(1) NOT NULL,
   `uploaded_url` tinytext NOT NULL,
@@ -135,10 +135,10 @@ CREATE TABLE IF NOT EXISTS `pmc_files` (
 -- --------------------------------------------------------
 
 --
--- 테이블 구조 `pmc_login_log`
+-- 테이블 구조 `intra_login_log`
 --
 
-CREATE TABLE IF NOT EXISTS `pmc_login_log` (
+CREATE TABLE IF NOT EXISTS `intra_login_log` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `ip_address` varchar(15) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `input_id` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
@@ -146,15 +146,26 @@ CREATE TABLE IF NOT EXISTS `pmc_login_log` (
   `auto_login` tinyint(1) NOT NULL,
   `login_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
+--
+-- 테이블의 덤프 데이터 `intra_login_log`
+--
+
+INSERT INTO `intra_login_log` (`id`, `ip_address`, `input_id`, `succeed`, `auto_login`, `login_time`) VALUES
+(1, '127.0.0.1', 'tester', 1, 1, '2013-07-29 03:06:44'),
+(2, '127.0.0.1', 'tester', 1, 1, '2013-07-29 06:45:59'),
+(3, '127.0.0.1', 'tester', 1, 1, '2013-07-29 06:47:12'),
+(4, '127.0.0.1', 'tester', 1, 1, '2013-07-29 06:52:59'),
+(5, '127.0.0.1', 'tester', 1, 1, '2013-07-29 06:53:49');
 
 -- --------------------------------------------------------
 
 --
--- 테이블 구조 `pmc_menu`
+-- 테이블 구조 `intra_menu`
 --
 
-CREATE TABLE IF NOT EXISTS `pmc_menu` (
+CREATE TABLE IF NOT EXISTS `intra_menu` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `title_locales` tinytext CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
@@ -171,24 +182,23 @@ CREATE TABLE IF NOT EXISTS `pmc_menu` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
 
 --
--- 테이블의 덤프 데이터 `pmc_menu`
+-- 테이블의 덤프 데이터 `intra_menu`
 --
 
-INSERT INTO `pmc_menu` (`id`, `title`, `title_locales`, `level`, `is_index`, `visible`, `parent_id`, `module`, `action`, `extra_vars`) VALUES
-(1, 'home', '{"en":"Home", "kr":"홈"}', 1, 1, 1, NULL, 'index', NULL, NULL),
-(2, 'notice', '{"en":"Notice", "kr":"공지사항"}', 1, 0, 1, NULL, 'board', NULL, NULL),
-(3, 'freeboard', '{"en":"Free Board", "kr":"자유게시판"}', 1, 0, 1, NULL, 'board', NULL, NULL),
-(4, 'others', '{"en":"Others", "kr":"기타"}', 1, 0, 1, NULL, 'page', NULL, NULL),
-(5, 'about', '{"en":"About", "kr":"정보"}', 2, 0, 1, 4, 'page', NULL, NULL),
-(6, 'libraries', '{"en":"Libraries", "kr":"라이브러리"}', 2, 0, 1, 4, 'page', NULL, NULL);
+INSERT INTO `intra_menu` (`id`, `title`, `title_locales`, `level`, `is_index`, `visible`, `parent_id`, `module`, `action`, `extra_vars`) VALUES
+(1, 'home', '홈', 1, 1, 1, NULL, 'index', NULL, NULL),
+(2, 'dormitory', '기숙사', 1, 0, 1, NULL, 'board', NULL, NULL),
+(3, 'cafeteria', '급식실', 1, 0, 1, NULL, 'board', NULL, NULL),
+(4, 'u-learning', 'U-러닝', 1, 0, 1, NULL, 'page', NULL, NULL),
+(5, 'els', '이러닝스튜디오', 1, 0, 1, 4, 'page', NULL, NULL);
 
 -- --------------------------------------------------------
 
 --
--- 테이블 구조 `pmc_session`
+-- 테이블 구조 `intra_session`
 --
 
-CREATE TABLE IF NOT EXISTS `pmc_session` (
+CREATE TABLE IF NOT EXISTS `intra_session` (
   `session_key` varchar(40) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `expire_time` datetime NOT NULL,
   `ip_address` varchar(15) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
@@ -199,21 +209,29 @@ CREATE TABLE IF NOT EXISTS `pmc_session` (
   KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- 테이블의 덤프 데이터 `intra_session`
+--
+
+INSERT INTO `intra_session` (`session_key`, `expire_time`, `ip_address`, `last_update`, `user_id`, `extra_vars`) VALUES
+('4a8ac55a04753495e16aaaaacaffd52c83d657f9', '2013-08-05 15:53:49', '127.0.0.1', '2013-07-29 06:53:49', 2, NULL),
+('abce8ffee4c5fede4f75f496f76d813176d310a9', '2013-08-05 12:06:44', '127.0.0.1', '2013-07-29 03:06:44', 2, NULL);
+
 -- --------------------------------------------------------
 
 --
--- 테이블 구조 `pmc_user`
+-- 테이블 구조 `intra_user`
 --
 
-CREATE TABLE IF NOT EXISTS `pmc_user` (
+CREATE TABLE IF NOT EXISTS `intra_user` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `input_id` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT 'SHA256',
   `password_salt` varchar(32) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT 'MD5',
-  `nick_name` varchar(40) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `user_type` varchar(1) NOT NULL,
   `user_name` varchar(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `email_address` varchar(60) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `phone_number` varchar(16) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `enable_mailing` tinyint(1) unsigned NOT NULL,
   `last_logined_ip` varchar(15) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `extra_vars` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
   PRIMARY KEY (`id`),
@@ -221,20 +239,36 @@ CREATE TABLE IF NOT EXISTS `pmc_user` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
--- 테이블의 덤프 데이터 `pmc_user`
+-- 테이블의 덤프 데이터 `intra_user`
 --
 
-INSERT INTO `pmc_user` (`id`, `input_id`, `password`, `password_salt`, `nick_name`, `user_name`, `email_address`, `phone_number`, `last_logined_ip`, `extra_vars`) VALUES
-(1, 'admin', '875bdbdd2cdb7326981de9c27bf9d76d52c75cd9bb1299417b1135b69a748b69', 'f98c94ebb87dc80be2a26991e3d5cc62', 'Admin', '어드민', 'admin@parmeter.kr', '010-1234-5678', '127.0.0.1', NULL),
-(2, 'tester', '2827e05770ec174da512daf5af4ce49f5e07209d82e2ed90b2ee565886e7b521', '8f4031bfc7640c5f267b11b6fe0c2507', '테스터', '테스터', 'tester@parameter.kr', '010-1234-5678', '127.0.0.1', NULL);
+INSERT INTO `intra_user` (`id`, `input_id`, `password`, `password_salt`, `user_type`, `user_name`, `email_address`, `enable_mailing`, `last_logined_ip`, `extra_vars`) VALUES
+(1, 'admin', '875bdbdd2cdb7326981de9c27bf9d76d52c75cd9bb1299417b1135b69a748b69', 'f98c94ebb87dc80be2a26991e3d5cc62', 's', '어드민', 'admin@parmeter.kr', 0, '127.0.0.1', NULL),
+(2, 'tester', '2827e05770ec174da512daf5af4ce49f5e07209d82e2ed90b2ee565886e7b521', '8f4031bfc7640c5f267b11b6fe0c2507', 's', '테스터', 'tester@parameter.kr', 0, '127.0.0.1', NULL);
 
 -- --------------------------------------------------------
 
 --
--- 테이블 구조 `pmc_user_group`
+-- 테이블 구조 `intra_user_auth_key`
 --
 
-CREATE TABLE IF NOT EXISTS `pmc_user_group` (
+CREATE TABLE IF NOT EXISTS `intra_user_auth_key` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `auth_key` varchar(10) NOT NULL,
+  `user_type` varchar(1) NOT NULL,
+  `join_data` text NOT NULL,
+  `email` varchar(40) DEFAULT NULL,
+  `email_key` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2012 ;
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `intra_user_group`
+--
+
+CREATE TABLE IF NOT EXISTS `intra_user_group` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` tinytext NOT NULL,
   `name_locales` text NOT NULL,
@@ -243,20 +277,20 @@ CREATE TABLE IF NOT EXISTS `pmc_user_group` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
--- 테이블의 덤프 데이터 `pmc_user_group`
+-- 테이블의 덤프 데이터 `intra_user_group`
 --
 
-INSERT INTO `pmc_user_group` (`id`, `name`, `name_locales`, `is_default`) VALUES
+INSERT INTO `intra_user_group` (`id`, `name`, `name_locales`, `is_default`) VALUES
 (1, 'admin', '{"en":"Admin Group", "kr":"관리그룹"}', 0),
 (2, 'general', '{"en":"General","kr":"일반회원"}', 1);
 
 -- --------------------------------------------------------
 
 --
--- 테이블 구조 `pmc_user_group_user`
+-- 테이블 구조 `intra_user_group_user`
 --
 
-CREATE TABLE IF NOT EXISTS `pmc_user_group_user` (
+CREATE TABLE IF NOT EXISTS `intra_user_group_user` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `group_id` int(10) unsigned NOT NULL,
   `user_id` int(10) unsigned NOT NULL,
@@ -266,44 +300,107 @@ CREATE TABLE IF NOT EXISTS `pmc_user_group_user` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
--- 테이블의 덤프 데이터 `pmc_user_group_user`
+-- 테이블의 덤프 데이터 `intra_user_group_user`
 --
 
-INSERT INTO `pmc_user_group_user` (`id`, `group_id`, `user_id`) VALUES
+INSERT INTO `intra_user_group_user` (`id`, `group_id`, `user_id`) VALUES
 (1, 1, 1),
 (2, 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `intra_user_parent`
+--
+
+CREATE TABLE IF NOT EXISTS `intra_user_parent` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `child_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `child_id` (`child_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=97 ;
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `intra_user_student`
+--
+
+CREATE TABLE IF NOT EXISTS `intra_user_student` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `grade` tinyint(1) unsigned NOT NULL,
+  `class` tinyint(1) unsigned NOT NULL,
+  `number` tinyint(2) unsigned NOT NULL,
+  `gender` varchar(1) NOT NULL COMMENT 'm/f',
+  `dormitory` tinyint(1) NOT NULL COMMENT '1:본관 2:학봉관',
+  `dormitory_room` varchar(7) NOT NULL,
+  `card_number` varchar(6) NOT NULL,
+  `image_url` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- 테이블의 덤프 데이터 `intra_user_student`
+--
+
+INSERT INTO `intra_user_student` (`id`, `user_id`, `grade`, `class`, `number`, `gender`, `dormitory`, `dormitory_room`, `card_number`, `image_url`) VALUES
+(1, 2, 1, 1, 50, 'm', 1, '100', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `intra_user_teacher`
+--
+
+CREATE TABLE IF NOT EXISTS `intra_user_teacher` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `department` int(11) unsigned NOT NULL,
+  `position` int(11) unsigned NOT NULL,
+  `homeroom_class` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `card_number` varchar(10) NOT NULL,
+  `subject_ids` tinytext,
+  PRIMARY KEY (`id`),
+  KEY `global_id` (`user_id`),
+  KEY `department` (`department`),
+  KEY `position` (`position`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `pmc_article`
+-- Constraints for table `intra_article`
 --
-ALTER TABLE `pmc_article`
-  ADD CONSTRAINT `pmc_article_ibfk_1` FOREIGN KEY (`board_id`) REFERENCES `pmc_board` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `pmc_article_ibfk_2` FOREIGN KEY (`writer_id`) REFERENCES `pmc_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `pmc_article_ibfk_3` FOREIGN KEY (`top_no`) REFERENCES `pmc_article` (`no`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `intra_article`
+  ADD CONSTRAINT `intra_article_ibfk_1` FOREIGN KEY (`board_id`) REFERENCES `intra_board` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `intra_article_ibfk_2` FOREIGN KEY (`writer_id`) REFERENCES `intra_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `intra_article_ibfk_3` FOREIGN KEY (`top_no`) REFERENCES `intra_article` (`no`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `pmc_article_comment`
+-- Constraints for table `intra_article_comment`
 --
-ALTER TABLE `pmc_article_comment`
-  ADD CONSTRAINT `pmc_article_comment_ibfk_1` FOREIGN KEY (`article_no`) REFERENCES `pmc_article` (`no`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `pmc_article_comment_ibfk_2` FOREIGN KEY (`writer_id`) REFERENCES `pmc_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `intra_article_comment`
+  ADD CONSTRAINT `intra_article_comment_ibfk_1` FOREIGN KEY (`article_no`) REFERENCES `intra_article` (`no`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `intra_article_comment_ibfk_2` FOREIGN KEY (`writer_id`) REFERENCES `intra_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `pmc_menu`
+-- Constraints for table `intra_menu`
 --
-ALTER TABLE `pmc_menu`
-  ADD CONSTRAINT `pmc_menu_ibfk_1` FOREIGN KEY (`parent_id`) REFERENCES `pmc_menu` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `intra_menu`
+  ADD CONSTRAINT `intra_menu_ibfk_1` FOREIGN KEY (`parent_id`) REFERENCES `intra_menu` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `pmc_user_group_user`
+-- Constraints for table `intra_user_group_user`
 --
-ALTER TABLE `pmc_user_group_user`
-  ADD CONSTRAINT `pmc_user_group_user_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `pmc_user_group` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `pmc_user_group_user_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `pmc_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `intra_user_group_user`
+  ADD CONSTRAINT `intra_user_group_user_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `intra_user_group` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `intra_user_group_user_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `intra_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- files/cache/127.0.0.1%2Fintranet/layout/%2Flayouts%2Fblank%2Flayout.html.compiled.php
+++
@@ -1,2 +0,0 @@
-<?php if (!defined('PMC')) exit; ?>
-<?php if ($func = getContent()) echo $func; ?>
