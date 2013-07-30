92910f9f80a53546d0965eb711857da9ab0ba379
Young Soo, Lee <prevdev@gmail.com>
2013-07-30 20:26:15 +0900

    add test/global.php

---
+++ TEMP/global.php
@@ -0,0 +1,26 @@
+<?php
+	
+	function theFunction1() {
+		/*
+			dasd
+			asd
+			asd
+			asd
+			asd
+			asd
+			a
+			dsa
+			d
+			sad
+
+
+			dasd
+			as
+			das
+			das
+			d
+			as
+		*/
+	}
+
+	
