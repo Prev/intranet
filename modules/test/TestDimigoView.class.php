9f7faa9a0ec58fd5383a7634e46ea545b007a951
Young Soo, Lee <prevdev@gmail.com>
2013-07-30 21:27:32 +0900

    파일 정리
    delete test module
    delete index credit view

--- modules/test/TestDimigoView.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-	class TestDimigoView extends View {
-
-		public function dispDimigo() {
-			var_dump2($this->model);
-			echo 'Dimigo!';
-		}
-
-	}
--- modules/test/TestModule.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-	// TestModule.class.php
-	class TestModule extends Module {
-		public function init() {
-			//echo 'module is initialized, action:';
-			//echo $this->action;
-		}
-	}
