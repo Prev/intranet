05f02449af9c6a659b9ace29879d47dad6324a05
Prev <prevdev@gmail.com>
2013-07-30 21:14:30 +0900

    Create database.php

---
+++ config/database.php
@@ -0,0 +1,24 @@
+<?php
+  
+	function getDBInfo() {
+		$dt = debug_backtrace();
+		$allow = (defined('ROOT_DIR') && ROOT_DIR . DIRECTORY_SEPARATOR . 'index.php' == $dt[0]['file']) ||
+					(defined('SSO_DIR') && SSO_DIR . DIRECTORY_SEPARATOR . 'conf.database.php' == $dt[0]['file']);
+		if (!$allow) {		
+			if (class_exists(Context))
+				Context::printWarning('SandBox error : call getDBInfo in other file');
+			else
+				throw new Exception('SandBox error : call getDBInfo in other file');
+			return;
+		}
+		
+		return (object) array(
+			'type' => 'mysql',
+			'host' => 'localhost',
+			'username' => 'root',
+			'password' => 'apmsetup',
+			'database_name' => 'pmc_test',
+			'prefix' => 'pmc_'
+		);
+	
+	}
