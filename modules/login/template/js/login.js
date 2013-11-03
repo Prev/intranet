6a38dd28c712c4620ba739200b7f5e1c9527f8bd
Prev (Lee Young Soo) <prevdev@gmail.com>
2013-11-03 20:22:10 +0900

    보안로그인 필수로 변경

--- modules/login/template/js/login.js
+++ modules/login/template/js/login.js
@@ -8,7 +8,6 @@ window.addEventListener("load", function () {
 	form = document.forms.login_form;
 	rsa = new RSA(rsaKeys.publicKey, null, rsaKeys.modulus);
 	
-	toggleSecureLogin(form.secure_login);
 	document.msCapsLockWarningOff = true;
 
 	form.id.focus();
@@ -29,36 +28,26 @@ function checkCapsLock(e) {
 		capslockAlert.style.visibility = 'hidden';
 }
 
-function toggleSecureLogin(target) {
-	if (target.checked)
-		form.action = form.action.split("procLogin").join("procSecureLogin");
-	else
-		form.action = form.action.split("procSecureLogin").join("procLogin");
-}
-
 function procLogin() {
+	if (!form) return;
+
 	try {
 		if (!form.id.value) { alert("아이디를 입력 해 주세요."); return false; }
 		if (!form.pw.value) { alert("비밀번호를 입력 해 주세요."); return false; }
 		
-		if (!form.secure_login.checked) {
-			form.submit();
-			return true;
-			
-		}else {
-			var enc_id = rsa.encrypt(form.id.value);
-			var enc_pw = rsa.encrypt(form.pw.value);
-			
-			form.enc_id.value = enc_id;
-			form.enc_pw.value = enc_pw;
-			form.check_sum.value = md5(form.id.value + form.pw.value);
-			
-			form.id.disabled = true;
-			form.pw.disabled = true;
+		var enc_id = rsa.encrypt(form.id.value);
+		var enc_pw = rsa.encrypt(form.pw.value);
+		
+		form.enc_id.value = enc_id;
+		form.enc_pw.value = enc_pw;
+		form.check_sum.value = md5(form.id.value + form.pw.value);
+		
+		form.id.disabled = true;
+		form.pw.disabled = true;
 
-			form.submit();
-			return true;
-		}
+		form.submit();
+		return true;
+		
 	}catch (e) {
 		alert("오류가 발생했습니다.");
 		console.dir(e);
